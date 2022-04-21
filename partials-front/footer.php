cac0b67fd9bc66750e48846f221eafaf2afc9887
Rajib Kumar Maity <57479702+Rajibcoder@users.noreply.github.com>
2022-04-21 10:33:37 +0530

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,22 @@
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/profile.php?id=100010167282627"><img src="images/fb_logo.png"></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/rajibkrmaity/"><img src="images/insta_logo.png"></a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/Rajibkrmaity"><img src="images/twit_logo.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+   <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By &copy<a href="https://www.facebook.com/profile.php?id=100010167282627">Rajib</a></p>
+        </div>
+    </section>
+</body>
+</html>
