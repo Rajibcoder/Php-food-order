0b84eab35058c0003d945175a0a97aa824dcad4d
Rajib Kumar Maity <57479702+Rajibcoder@users.noreply.github.com>
2022-04-21 10:16:51 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,40 @@
+<?php include("partials-front/menu.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="css/contact.css">
+</head>
+<body>
+<section id="contact" class="contact">
+  
+  <h1 class="section-header">Contact</h1>
+  
+  <div class="contact-wrapper">
+    
+      <div class="direct-contact-container">
+
+        <ul class="contact-list">
+          <li class="list-item"><img src="images/map.png" width="100px"><br><span class="contact-text place">Ramnagar, Purba Medinipur, West Bengal</span></i></li>
+          
+          <li class="list-item"><img src="images/phone.jpg"  width="100px"><br><span class="contact-text phone"><a href="tel:1-212-555-5555" title="Give me a call">+91 8670580980</a></span></i></li>
+          
+          <li class="list-item"><img src="images/email.png"  width="100px"><br>x<span class="contact-text gmail"><a href="mailto:#" title="Send me an email">gitafirstfood@outlook.com</a></span></i></li>
+          
+        </ul>
+
+      </div>
+    
+  </div>
+  
+</section>  
+  
+  
+</body>
+</html>
+
+<?php include("partials-front/footer.php"); ?>
